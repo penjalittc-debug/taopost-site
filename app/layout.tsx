eba69a5a4bd4f75a4c1769336a22e362989b09ed
Ali Azizov <penjalittc@gmail.com>
2026-04-07 08:35:11 +0300

    analytics: add Yandex Metrika counter 108426590

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -58,6 +59,20 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body>
+        <Script id="yandex-metrika" strategy="afterInteractive">{`
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108426590', 'ym');
+          ym(108426590, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+        `}</Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/108426590" style={{position:'absolute', left:'-9999px'}} alt="" />
+          </div>
+        </noscript>
         {children}
       </body>
     </html>
